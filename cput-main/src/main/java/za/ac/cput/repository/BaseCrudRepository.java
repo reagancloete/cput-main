fe38885ec45ec408a87aa6b30953d9e1e8358cd9
lelam valisi <128606673+lelamvalisi@users.noreply.github.com>
2024-10-20 22:19:14 +0200

    Update BaseCrudRepository.java

--- cput-main/src/main/java/za/ac/cput/repository/BaseCrudRepository.java
+++ cput-main/src/main/java/za/ac/cput/repository/BaseCrudRepository.java
@@ -4,6 +4,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.querydsl.QuerydslPredicateExecutor;
 import org.springframework.data.repository.NoRepositoryBean;
 
+
 @NoRepositoryBean
 public interface BaseCrudRepository<E, K> extends JpaRepository<E, K> , QuerydslPredicateExecutor<E>  {
 }
