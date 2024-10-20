a638ffa507c4a5a341d7024c618f3373d4257784
lelam valisi <128606673+lelamvalisi@users.noreply.github.com>
2024-10-20 22:26:56 +0200

    Update CustomerService.java

--- cput-main/src/main/java/za/ac/cput/service/CustomerService.java
+++ cput-main/src/main/java/za/ac/cput/service/CustomerService.java
@@ -6,6 +6,7 @@ import za.ac.cput.domain.Customer;
 public interface CustomerService extends BaseCrudService<Customer, Long>{
     Customer create(CustomerRequest form);
     
+    
 
     Customer update(Long id, CustomerRequest form);
 
