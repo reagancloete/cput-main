9f2c2f6ec019d53da80b3395d0ebe2c2c9cca290
lelam valisi <128606673+lelamvalisi@users.noreply.github.com>
2024-10-20 22:28:32 +0200

    Update UserService.java

--- cput-main/src/main/java/za/ac/cput/service/UserService.java
+++ cput-main/src/main/java/za/ac/cput/service/UserService.java
@@ -4,6 +4,7 @@ package za.ac.cput.service;
 import za.ac.cput.dto.request.UserRequest;
 import za.ac.cput.domain.User;
 
+
 public interface UserService extends BaseCrudService<User, Long>{
     User create(UserRequest form);
 
